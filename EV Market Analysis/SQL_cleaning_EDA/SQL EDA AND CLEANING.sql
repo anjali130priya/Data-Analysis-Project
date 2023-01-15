47df5155cfd5f5461a87515b5274d2e5584d010d
ANJALI PRIYA <69565322+anjali130priya@users.noreply.github.com>
2023-01-15 09:38:51 +0530

    Update SQL EDA AND CLEANING.sql

--- EV Market Analysis/SQL_cleaning_EDA/SQL EDA AND CLEANING.sql
+++ EV Market Analysis/SQL_cleaning_EDA/SQL EDA AND CLEANING.sql
@@ -1,1 +1,8 @@
 
+SELECT created_at, 
+	COUNT(CASE WHEN event_name = 'post' 1 ELSE null END) * 1.00 / 
+	COUNT (CASE WHEN event_name = 'enter' 1 ELSE null END) * 100 AS percent_success
+FROM post_events
+GROUP BY created_at
+ORDER BY created_at;
+
