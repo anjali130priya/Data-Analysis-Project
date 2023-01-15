2eb3d329d5016e2235c3eaba69d2c31989cc860c
ANJALI PRIYA <69565322+anjali130priya@users.noreply.github.com>
2023-01-15 09:41:34 +0530

    Update SQL EDA AND CLEANING.sql

--- EV Market Analysis/SQL_cleaning_EDA/SQL EDA AND CLEANING.sql
+++ EV Market Analysis/SQL_cleaning_EDA/SQL EDA AND CLEANING.sql
@@ -8,3 +8,10 @@ from charging_facility_avail
 
 select *
 from charging_sanc_stn
+
+
+-- REMOVING AND CHECKING WHITE SPACES
+
+select length("States") as l1, length(trim("States"))  as l2
+from location_ev
+where length("States") !=  length(trim("States"))
